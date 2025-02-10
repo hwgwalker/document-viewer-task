d23c8fd51d1ce0f2d751b68083ba3cbcb41efefc
Henry Walker <henry@uxsequence.io>
2025-02-10 16:55:10 +0000

    create test for table data rendering

--- app/_components/FileExplorer/FileExplorer.test.tsx
+++ app/_components/FileExplorer/FileExplorer.test.tsx
@@ -1,6 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import { FileExplorer } from "./FileExplorer";
 
+const mockData = [
+  {
+    type: "pdf",
+    name: "Employee Handbook",
+    added: "2017-01-06",
+  },
+];
+
 describe("FileExplorer", () => {
   /**
    *  Rendering
@@ -12,6 +20,16 @@ describe("FileExplorer", () => {
       const table = screen.getByRole("table");
 
       expect(table).toBeInTheDocument();
+    });
+
+    it("Should render td elements with correct content", () => {
+      render(<FileExplorer data={mockData} />);
+
+      const tdElements = screen.getAllByRole("cell");
+
+      expect(tdElements[0]).toHaveTextContent(/pdf$/i);
+      expect(tdElements[1]).toHaveTextContent(/employee handbook$/i);
+      expect(tdElements[2]).toHaveTextContent("2017-01-06");
     });
   });
 });
