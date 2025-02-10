07409fa0b6b4faddea894b9afb4f798c2df3374f
Henry Walker <henry@uxsequence.io>
2025-02-10 16:48:24 +0000

    add headers to file explorer

--- app/_components/FileExplorer/FileExplorer.tsx
+++ app/_components/FileExplorer/FileExplorer.tsx
@@ -1,5 +1,15 @@
 export function FileExplorer() {
-    return (
-        <table></table>
-    )
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>File Type</th>
+          <th>Name</th>
+          <th>Date Added</th>
+        </tr>
+      </thead>
+
+      <tbody></tbody>
+    </table>
+  );
 }
