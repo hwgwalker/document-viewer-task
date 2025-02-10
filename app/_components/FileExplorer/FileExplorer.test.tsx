e199b35c6e956c97135dfad695f09d82e5185a5a
Henry Walker <henry@uxsequence.io>
2025-02-10 17:48:22 +0000

    add test to show folders/files

--- app/_components/FileExplorer/FileExplorer.test.tsx
+++ app/_components/FileExplorer/FileExplorer.test.tsx
@@ -1,11 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import { FileExplorer } from "./FileExplorer";
+import { FileType } from "@/app/_types/FileDataType";
 
 const mockData = [
   {
     type: "pdf",
     name: "Employee Handbook",
     added: "2017-01-06",
+  },
+  {
+    type: "folder",
+    name: "Expenses",
+    files: [
+      {
+        type: "doc",
+        name: "Expenses claim form",
+        added: "2017-05-02",
+      },
+    ],
   },
 ];
 
@@ -30,6 +42,19 @@ describe("FileExplorer", () => {
       expect(tdElements[0]).toHaveTextContent(/pdf$/i);
       expect(tdElements[1]).toHaveTextContent(/employee handbook$/i);
       expect(tdElements[2]).toHaveTextContent("2017-01-06");
+    });
+
+    it("Should render files and folders correctly", () => {
+      render(<FileExplorer data={mockData} />);
+
+      // file
+      expect(screen.getByText(/pdf$/i)).toBeInTheDocument();
+      expect(screen.getByText(/employee handbook$/i)).toBeInTheDocument();
+      expect(screen.getByText("2017-01-06")).toBeInTheDocument();
+
+      // folder
+      expect(screen.getByText("📁")).toBeInTheDocument();
+      expect(screen.getByText(/expenses$/i)).toBeInTheDocument();
     });
   });
 });
